449e4805f87cbbea9e4354459366b87d2ecf24c6
EAimTY <EAimTY@users.noreply.github.com>
2024-01-30 23:47:54 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use crate::device::{DeviceAdaptor, EmulatedDevice, HardwareDevice};
+use std::{sync::Arc, thread};
+
+mod device;
+mod poll;
+
+#[derive(Clone)]
+pub struct Device(Arc<DeviceInner<dyn DeviceAdaptor>>);
+
+struct DeviceInner<D: ?Sized> {
+    #[allow(unused)]
+    is_emulated: bool,
+
+    adaptor: D,
+}
+
+impl Device {
+    pub fn new_emulated() -> Self {
+        let inner = Arc::new(DeviceInner {
+            is_emulated: true,
+            adaptor: EmulatedDevice::init(),
+        });
+
+        let dev = Self(inner);
+
+        let dev_for_poll_ctrl_rb = dev.clone();
+        let dev_for_poll_work_rb = dev.clone();
+
+        thread::spawn(move || dev_for_poll_ctrl_rb.poll_ctrl_rb());
+        thread::spawn(move || dev_for_poll_work_rb.poll_work_rb());
+
+        dev
+    }
+
+    pub fn new_hardware() -> Self {
+        let inner = Arc::new(DeviceInner {
+            is_emulated: true,
+            adaptor: HardwareDevice::init(),
+        });
+
+        let dev = Self(inner);
+
+        let dev_for_poll_ctrl_rb = dev.clone();
+        let dev_for_poll_work_rb = dev.clone();
+
+        thread::spawn(move || dev_for_poll_ctrl_rb.poll_ctrl_rb());
+        thread::spawn(move || dev_for_poll_work_rb.poll_work_rb());
+
+        dev
+    }
+}
